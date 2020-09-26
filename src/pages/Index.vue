aa7ed5b6d527986ac6305dcc125e7f49a9165522
Lewandy Dilone Bonifacio <lewandydilone1@live.com>
2020-09-26 19:03:08 -0400

    Add register visitor page features

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,13 +1,49 @@
 <template>
-  <q-page class="flex flex-center">
-    <div>
-      Agile visitors
+  <div class="q-pa-md">
+    <div class="row q-gutter-md">
+      <div class="col">
+        <label class="text-weight-bold text-h4" for="">Visitor</label>
+
+          <q-input label="Visitor code" v-model="form.visitor.code" />
+          <q-input label="Full name" v-model="form.visitor.fullName" />
+          <q-input label="Title / Position" v-model="form.visitor.title" />
+          <q-input
+            v-model="form.visitor.reasonForVisit"
+            label="Reason for visit"
+            filled
+            type="textarea"
+          />
+      </div>
+      <div class="col">
+        <label class="text-weight-bold text-h4" for="">Receiver</label>
+
+        <q-input label="Receiver code" v-model="form.receiver.code" />
+          <q-input label="Full name" v-model="form.receiver.fullName" />
+          <q-input label="Title / Position" v-model="form.receiver.title" />
+      </div>
     </div>
-  </q-page>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+  data () {
+    return {
+      form: {
+        visitor: {
+          code: '',
+          fullName: '',
+          title: '',
+          reasonForVisit: ''
+        },
+        receiver: {
+          code: '',
+          fullName: '',
+          title: ''
+        }
+      }
+    }
+  }
 }
 </script>
