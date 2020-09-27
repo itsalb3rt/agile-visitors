04a29af40654c46ca38bb8944260f0ddf2ad88b9
Albert E. Hidalgo Taveras <itsalb3rt@gmail.com>
2020-09-26 21:40:21 -0400

    feat
    form is ready for send and auto fill data from server

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,51 +1,168 @@
 <template>
   <div class="q-pa-md">
-    <div class="row q-gutter-md">
-      <div class="col-12 col-md-5 col-sm-12">
-        <label class="text-weight-bold text-h4" for="">Visitor</label>
+    <q-form @submit="createvisit()" class="q-gutter-md">
+      <div class="row q-gutter-md">
+        <div class="col-12 col-md-5 col-sm-12">
+          <label class="text-weight-bold text-h4" for="">Visitor</label>
 
-          <q-input label="Visitor code" v-model="form.visitor.code" />
-          <q-input label="Full name" v-model="form.visitor.fullName" />
-          <q-input label="Title / Position" v-model="form.visitor.title" />
           <q-input
-            v-model="form.visitor.reasonForVisit"
+            label="Visitor code"
+            v-model="form.visitor.code"
+            :rules="formRulesMixin_requiredInput"
+            debounce="500"
+            :loading="loading.visitor"
+            @input="requestVisitorByCode()"
+          />
+          <q-input
+            label="Full name"
+            v-model="form.visitor.fullName"
+            :rules="formRulesMixin_requiredInput"
+          />
+          <q-input
+            label="Title / Position"
+            v-model="form.visitor.titlePosition"
+            :rules="formRulesMixin_requiredInput"
+          />
+          <q-input
+            v-model="form.reasonVisit"
             label="Reason for visit"
             filled
             type="textarea"
+            :rules="formRulesMixin_requiredInput"
           />
-      </div>
-      <div class="col-12 col-md-5 col-sm-12">
-        <label class="text-weight-bold text-h4" for="">Receiver</label>
+        </div>
+        <div class="col-12 col-md-5 col-sm-12">
+          <label class="text-weight-bold text-h4" for="">Receiver</label>
 
-        <q-input label="Receiver code" v-model="form.receiver.code" />
-          <q-input label="Full name" v-model="form.receiver.fullName" />
-          <q-input label="Title / Position" v-model="form.receiver.title" />
+          <q-input
+            label="Receiver code"
+            v-model="form.receiver.code"
+            :rules="formRulesMixin_requiredInput"
+            debounce="500"
+            :loading="loading.receiver"
+            @input="requestReceiverByCode()"
+          />
+          <q-input
+            label="Full name"
+            v-model="form.receiver.fullName"
+            :rules="formRulesMixin_requiredInput"
+          />
+          <q-input
+            label="Title / Position"
+            v-model="form.receiver.titlePosition"
+            :rules="formRulesMixin_requiredInput"
+          />
+        </div>
       </div>
-    </div>
-    <div class="row q-mt-lg">
-      <q-btn color="primary" icon="save" label="SAVE" />
-    </div>
+      <div class="row q-mt-lg">
+        <q-btn color="primary" icon="save" label="SAVE" type="submit" />
+      </div>
+    </q-form>
   </div>
 </template>
 
 <script>
+import formMixin from 'src/mixins/FormRules'
+
 export default {
   name: 'PageIndex',
+  mixins: [formMixin],
   data () {
     return {
       form: {
         visitor: {
           code: '',
           fullName: '',
-          title: '',
-          reasonForVisit: ''
+          titlePosition: ''
         },
         receiver: {
           code: '',
           fullName: '',
-          title: ''
-        }
+          titlePosition: ''
+        },
+        reasonVisit: ''
+      },
+      loading: {
+        visitor: false,
+        receiver: false
       }
+    }
+  },
+  methods: {
+    createvisit () {
+      this.$store
+        .dispatch('visits/create', this.form)
+        .then(response => {
+          this.form.visitor.code = ''
+          this.form.visitor.fullName = ''
+          this.form.visitor.titlePosition = ''
+          this.form.titlePosition = ''
+          this.$q.notify({
+            type: 'positive',
+            message: 'Visit register.'
+          })
+        })
+        .catch(error => {
+          console.log('createvisit -> error', error)
+          this.$q.notify({
+            type: 'negative',
+            message: 'Internal server error, try again...'
+          })
+        })
+    },
+    requestVisitorByCode () {
+      this.loading.visitor = true
+      this.$store
+        .dispatch('users/getUsers', `?code=${this.form.visitor.code}`)
+        .then(response => {
+          if (response.data.data.length > 0) {
+            const visitor = response.data.data[0]
+            this.form.visitor.fullName = visitor.fullName
+            this.form.visitor.titlePosition = visitor.titlePosition
+            this.$q.notify({
+              type: 'positive',
+              message: 'Visitor existis.'
+            })
+          } else {
+            this.$q.notify({
+              type: 'negative',
+              message: 'Visitor not existis.'
+            })
+          }
+        })
+        .catch(error => {
+          console.log(error)
+        })
+        .finally(() => {
+          this.loading.visitor = false
+        })
+    },
+    requestReceiverByCode () {
+      this.loading.receiver = true
+      this.$store
+        .dispatch('users/getUsers', `?code=${this.form.receiver.code}`)
+        .then(response => {
+          if (response.data.data.length > 0) {
+            const receiver = response.data.data[0]
+            this.form.receiver.fullName = receiver.fullName
+            this.form.receiver.titlePosition = receiver.titlePosition
+            this.$q.notify({
+              type: 'positive',
+              message: 'Receiver existis.'
+            })
+          } else {
+            this.$q.notify({
+              type: 'negative',
+              message: 'Receiver not existis.'
+            })
+          }
+        })
+        .catch(error => {
+          console.log(error)
+        })
+        .finally(() => {
+          this.loading.receiver = false
+        })
     }
   }
 }
