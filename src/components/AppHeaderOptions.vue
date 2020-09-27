ee2f3e1849d0ab536a85262ce15c577c76d0bb24
Lewandy Dilone Bonifacio <lewandydilone1@live.com>
2020-09-27 19:53:06 -0400

    Add app header options component

---
+++ src/components/AppHeaderOptions.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="q-pa-md" style="display: inline-block">
+    <q-btn-dropdown icon="menu" color="primary">
+      <q-list>
+        <q-item clickable v-close-popup @click="() => $router.push('/')">
+          <q-item-section>
+            <q-item-label>Register visit</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item clickable v-close-popup @click="() => $router.push('/reports')">
+          <q-item-section>
+            <q-item-label>Reports</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-btn-dropdown>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
