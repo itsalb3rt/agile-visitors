cbe385004c7045b9833782f9c1d9c0304b69b837
Albert E. Hidalgo Taveras <itsalb3rt@gmail.com>
2020-09-26 20:16:44 -0400

    feat
    added API var

--- quasar.conf.js
+++ quasar.conf.js
@@ -7,7 +7,7 @@
 // https://quasar.dev/quasar-cli/quasar-conf-js
 /* eslint-env node */
 
-module.exports = function (/* ctx */) {
+module.exports = function (ctx) {
   return {
     // https://quasar.dev/quasar-cli/supporting-ts
     supportTS: false,
@@ -46,7 +46,11 @@ module.exports = function (/* ctx */) {
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
       vueRouterMode: 'history', // available values: 'hash', 'history'
-
+      env: {
+        API: ctx.dev
+          ? 'http://localhost:5000/api'
+          : 'https://prod.api.com'
+      },
       // transpile: false,
 
       // Add dependencies for transpiling with Babel (Array of string/regex)
