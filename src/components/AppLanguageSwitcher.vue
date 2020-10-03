032ff12cf3e2cb48c8f0afe75f26de57fa0e74fc
Lewandy Dilone <lewandydilone1@live.com>
2020-10-03 18:49:30 -0400

    App language switcher component added

---
+++ src/components/AppLanguageSwitcher.vue
@@ -0,0 +1,43 @@
+<template>
+  <q-select
+    v-model="lang"
+    label-color="white"
+    :options="langOptions"
+    :label="$t('language')"
+    borderless
+    emit-value
+    map-options
+    style="min-width: 120px;"
+  >
+    <template v-slot:selected-item="scope">
+          <q-chip
+            dense
+            :tabindex="scope.tabindex"
+            color="primary"
+            class="q-ma-none"
+          >
+            <q-avatar :icon="scope.opt.icon" />
+            <span style="color:white">{{ scope.opt.label }}</span>
+          </q-chip>
+        </template>
+  </q-select>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      lang: this.$i18n.locale,
+      langOptions: [
+        { value: 'en-us', label: 'English', icon: '🇺🇸' },
+        { value: 'es', label: 'Spanish', icon: '🇪🇸' }
+      ]
+    }
+  },
+  watch: {
+    lang (lang) {
+      this.$i18n.locale = lang
+    }
+  }
+}
+</script>
